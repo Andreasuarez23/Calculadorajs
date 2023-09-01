0748e7487359aa4fbfed9c0a90d64328bdb7197e
andrea suarez <andrea.suarez@itscordoba.edu.ar>
2023-09-01 16:21:04 -0300

    modificacion en archivo

--- Calculadora _js/java/main.js
+++ Calculadora _js/java/main.js
@@ -1,7 +1,27 @@
-/* llamamos a las clases pantalla y botones
-*/
-const pantalla = document.querySelector(".pantalla");
-const botones = document.querySelectorAll(".btn")
+
+// decalracion de variables para los botones
+const pantalla = document.querySelector('.pantalla');
+const valorActual = document.getElementById('valorActual');
+const borrar = document.getElementById('borrar');
+const volver = document.getElementById('volver');
+const dividir = document.getElementById('dividir');
+const siete = document.getElementById('siete');
+const ocho = document.getElementById('ocho');
+const nueve = document.getElementById('nueve');
+const multiplicar = document.getElementById('multiplicar');
+const cuatro = document.getElementById('cuatro');
+const cinco = document.getElementById('cinco');
+const seis = document.getElementById('seis');
+const restar = document.getElementById('restar');
+const uno = document.getElementById('uno');
+const dos = document.getElementById('dos');
+const tres = document.getElementById('tres');
+const sumar = document.getElementById('sumar');
+const igual = document.getElementById('igual');
+const cero = document.getElementById('cero');
+
+let valorAnterior = null;
+let operacionActual = null;
 
 /* a cada boton le agregamos un escuchador de evento*/
 botones.forEach(boton =>{
@@ -18,13 +38,13 @@ botones.forEach(boton =>{
             } else {
                 pantalla.textContent.slice(0,-1);
             }
-                
+             console.log(boton.id);   
             return;
 
         }
         if (boton.id === "igual"){
             try{
-                pantalla.textContent = eval(pantalla.textContent);
+                pantalla.textContent = "igual"
 
             }catch{
                 pantalla.textContent ="error";
@@ -45,3 +65,70 @@ botones.forEach(boton =>{
 
     })
 })
+    function suma (a,b){
+        a + b ;
+}
+    function resta (a,b){
+        a - b;
+}
+    function multiplicacion (a,b){
+        a * b;
+}
+    function division (a,b){
+        a / b ;
+}
+
+
+
+// Eventos de los botones de operación
+sumar.addEventListener('click', () => {
+    operacionActual = 'suma';
+    valorAnterior = Number(valorActual.innerText);
+    pantalla.innerText = `${valorAnterior} + `;
+    valorActual.innerText = '';
+  });
+  
+  restar.addEventListener('click', () => {
+    operacionActual = 'resta';
+    valorAnterior = Number(valorActual.innerText);
+    pantalla.innerText = `${valorAnterior} - `;
+    valorActual.innerText = '';
+  });
+  
+  multiplicar.addEventListener('click', () => {
+    operacionActual = 'multiplicacion';
+    valorAnterior = Number(valorActual.innerText);
+    pantalla.innerText = `${valorAnterior} * `;
+    valorActual.innerText = '';
+  });
+  
+  dividir.addEventListener('click', () => {
+    operacionActual = 'division';
+    valorAnterior = Number(valorActual.innerText);
+    pantalla.innerText = `${valorAnterior} / `;
+    valorActual.innerText = '';
+
+// Evento para los botones numéricos
+cero.addEventListener('click', () => {
+    valorActual.innerText += '0';
+  });
+  
+  uno.addEventListener('click', () => {
+    valorActual.innerText += '1';
+  });
+  
+
+
+
+
+
+
+  // Evento para el botón de igual
+igual.addEventListener('click', () => {
+    const valorActualNum = Number(valorActual.innerText);
+    let resultado;
+  
+    if (operacionActual === 'suma') {
+      resultado = suma(valorAnterior, valorActualNum);
+    } else if (operacionActual === 'resta') {
+      resultado = resta(valorAnterior, valorActualNum);
